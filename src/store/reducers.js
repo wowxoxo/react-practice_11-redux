acf5acd65464ad5b34b60a011c06d220e977d583
Petr Tsivinsky <petr.tsivinsky@iitrust.ru>
2022-03-09 12:39:52 +0300

    feat: added custom combineReducers func

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,7 +1,24 @@
-import { combineReducers } from "redux";
+// import { combineReducers } from "redux";
 import { loginReducer } from "./login/reducers";
+
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce((nextState, key) => {
+      nextState[key] = reducers[key](state[key], action);
+      return nextState;
+    }, {});
+  };
+};
 
 export default combineReducers({
   // auth: authReducer,
   login: loginReducer
 });
+
+const arr = [1, 2, 3, 4, 5];
+
+const result = arr.reduce(function (sum, current) {
+  return sum + current;
+}, 0);
+
+console.log(result);
